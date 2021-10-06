cafdb19ce7d10696751422390a6170df5cf3aeca
NicWille <nic.wille1020@yahoo.com>
2021-10-06 11:13:24 -0700

    inquirer working

--- app.js
+++ app.js
@@ -1,14 +1,33 @@
-const fs = require('fs')
-const generatePage = require('./src/page-template.js')
+const inquirer = require('inquirer')
 
-const profileDataArgs = process.argv.slice(2)
+inquirer
+  .prompt([
+        /* Pass your questions in here */
+    {
+      type: 'input',
+      name: 'name',
+      message: 'What is your name?'
+    }
 
-const [name, github] = profileDataArgs
+  ])
+  .then((answers) => {
+    // Use user feedback for... whatever!!
+    console.log(answers)
+  })
 
-fs.writeFile('index.html', generatePage(name, github), err =>  {
-  if (err) throw err
 
-  console.log("portfolio complete")
-})
+
+
+
+// const fs = require('fs')
+// const generatePage = require('./src/page-template.js')
+
+// const pageHTML = generatePage(name, github)
+
+// fs.writeFile('./index.html', pageHTML, err =>  {
+//   if (err) throw err
+
+//   console.log("portfolio complete")
+// })
 
 
